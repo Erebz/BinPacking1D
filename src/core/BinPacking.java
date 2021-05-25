8f308a2aefce639feb418862fb3e206c6e6a9ee0
erebz <ebobow@gmail.com>
2021-05-25 19:46:24 +0200

    Question 3 : a) OK

--- src/core/BinPacking.java
+++ src/core/BinPacking.java
@@ -1,5 +1,8 @@
 package core;
 
+import com.google.ortools.Loader;
+import com.google.ortools.linearsolver.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -67,10 +70,14 @@ public class BinPacking {
         return bins.size();
     }
 
+    private void reset() {
+        bins.clear();
+    }
 
 
     //Algorithmes de résolution
     public void firstFitDecreasing(){
+        this.reset();
         this.trierItemsDecroissant();
         for(Item item : items){
             boolean stop = false;
@@ -91,6 +98,76 @@ public class BinPacking {
         }
     }
 
+
+    public void linearProgrammingResolution(){
+        this.reset();
+        int nbItems = items.size();
+        int nbBinsMax = nbItems;
+        Loader.loadNativeLibraries();
+        MPSolver solver = MPSolver.createSolver("SCIP"); //GLOP, SCIP
+
+        //Variables
+        MPVariable[][] x = new MPVariable[nbItems][nbBinsMax];
+        for (int i = 0; i < nbItems; i++) {
+            for (int j = 0; j < nbBinsMax; j++) {
+                String s = "X[" + i + "," + j +"]";
+                x[i][j] = solver.makeBoolVar(s);
+            }
+        }
+        MPVariable[] y = new MPVariable[nbBinsMax];
+        for (int j = 0; j < nbBinsMax; j++) {
+            String s = "Y" + j;
+            y[j] = solver.makeBoolVar(s);
+        }
+
+        //Contraintes
+        //Pour chaque  item i : somme(j=1 à nbBinsMax)[Xij] = 1
+        for (int i = 0; i < nbItems; i++) {
+            MPConstraint c = solver.makeConstraint(1.0, 1.0, "");
+            for (int j = 0; j < nbBinsMax; j++) {
+                c.setCoefficient(x[i][j], 1);
+            }
+        }
+
+        //Pour chaque  bin j : somme(i=1 à nbItems)[Xij * Ti] <= TailleBinMax
+        double infinity = Double.POSITIVE_INFINITY;
+        for (int j = 0; j < nbBinsMax; j++) {
+            //MPConstraint c = solver.makeConstraint(-infinity, tailleBin, "");
+            MPConstraint constraint = solver.makeConstraint(0, infinity, "");
+            constraint.setCoefficient(y[j], tailleBin);
+            for (int i = 0; i < nbItems; i++) {
+                //c.setCoefficient(x[i][j], items.get(i).getTaille());
+                constraint.setCoefficient(x[i][j], -items.get(i).getTaille());
+            }
+        }
+
+        //Objectif
+        MPObjective objective = solver.objective();
+        for (int j = 0; j < nbBinsMax; j++) {
+            objective.setCoefficient(y[j], 1);
+        }
+        objective.setMinimization();
+
+        //Résolution
+        final MPSolver.ResultStatus resultat = solver.solve();
+        if (resultat == MPSolver.ResultStatus.OPTIMAL) {
+            //System.out.println("[Solution optimale] nbBins = " + objective.value());
+            for (int j = 0; j < nbBinsMax; j++) {
+                if (y[j].solutionValue() == 1) {
+                    Bin bin = new Bin(tailleBin);
+                    for (int i = 0; i < nbItems; i++) {
+                        if (x[i][j].solutionValue() == 1) {
+                            bin.ajouterItem(items.get(i));
+                        }
+                    }
+                    bins.add(bin);
+                }
+            }
+        } else {
+            System.err.println("Pas de solution optimale.");
+        }
+
+    }
 
 
     @Override
--- src/core/Main.java
+++ src/core/Main.java
@@ -4,15 +4,29 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File dossierData = new File("./data");
+        /*File dossierData = new File("./data");
         for(File f : dossierData.listFiles()){
             String data = f.getName();
             BinPacking bp = new BinPacking(data);
             System.out.print(bp);
             System.out.print("> FirstFitDecreasing ... ");
             bp.firstFitDecreasing();
-            System.out.print(bp.getNbBins() + "bins");
+            System.out.println(bp.getNbBins() + "bins");
+            System.out.print("> Résolution programmation linéaire ... ");
+            bp.linearProgrammingResolution();
+            System.out.println(bp.getNbBins() + "bins");
             System.out.println("\n");
-        }
+        }*/
+
+        String data = "binpack1d_00.txt";
+        BinPacking bp = new BinPacking(data);
+        System.out.print(bp);
+        System.out.print("> FirstFitDecreasing ... ");
+        bp.firstFitDecreasing();
+        System.out.println(bp.getNbBins() + "bins");
+        System.out.print("> Résolution programmation linéaire ... ");
+        bp.linearProgrammingResolution();
+        System.out.println(bp.getNbBins() + "bins");
+        System.out.println("\n");
     }
 }
